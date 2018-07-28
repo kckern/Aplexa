707e5efd6e8bfb11c016509c73f1cc8838fc7f62
KC Kern <kern.kc@bookofmormoncentral.org>
2018-07-28 20:31:56 +0900

    Build 14: http://jenkins.kckern.net/job/Aplexa/14/console

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ cd $DIR
 /usr/local/bin/tree ~/.ssh
 /bin/pwd
 /usr/bin/git add .
-/usr/bin/git commit  -m 'Build $1: ${2}console' 
+/usr/bin/git commit  -m "Build $1: ${2}console"
 /usr/bin/git  push
 /usr/local/bin/ask deploy
