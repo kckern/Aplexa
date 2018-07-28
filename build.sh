34ef15ab5ede29ee7c96752a35cdd4fff9500c18
KC Kern <kern.kc@bookofmormoncentral.org>
2018-07-28 20:14:45 +0900

    Build $1: ${2}console

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 PATH="/Users/kckern/Dropbox/Backups/Code/Aplexa"
 cd $PATH
+tree ~/.ssh
 /bin/pwd
 /usr/bin/git add .
 /usr/bin/git commit  -m 'Build $1: ${2}console' 
 /usr/bin/git  push
-/usr/local/bin/ask deploy
+ask deploy
